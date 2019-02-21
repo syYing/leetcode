08aa90053946d1a55dcf08d2ac985853aa087ca5
syYing <706580680@qq.com>
2019-02-21 13:32:25 +0800

    Hard

---
+++ Array/9-First Missing Positive.cpp
@@ -0,0 +1,32 @@
+
+#include <vector>
+using namespace std;
+
+/**
+ * Given an unsorted integer array, find the smallest missing positive integer.
+ * Note:Your algorithm should run in O(n) time and uses constant extra space.
+ */
+
+class Solution {
+public:
+    /**
+     * Given a number in the array:
+     * if it is non-positive, ignore it;
+     * if it is positive, say we have nums[i] = x, we know it should be in slot nums[x - 1].
+     * That is to say, we can swap nums[i] with nums[x - 1] so as to place x into the right place.
+     * We need to keep swaping until all numbers are either non-positive or in the right place.
+     */
+    int firstMissingPositive(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            while(nums[i] > 0 && nums[i] <= nums.size() && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
+            }
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.size() + 1;
+    }
+};
